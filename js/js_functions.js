a604993c94d674bebb8c5f18360d4f2fd70f482b
roblinthegoblin <76852565+roblinthegoblin@users.noreply.github.com>
2022-02-20 22:51:26 +0000

    Started adding text to expertise section
    Started adding text to expertise and bio pages

---
+++ js/js_functions.js
@@ -0,0 +1,11 @@
+//<script>document.write(new Date().getFullYear())</script>
+
+/*
+function welcomeR(name){
+    return "Welcome " name "!";
+}
+*/
+
+function currentYear(){
+	return new Date().getFullYear()
+}
